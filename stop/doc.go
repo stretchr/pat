69a43435a36fc12f8e67f9688239e7583100c955
Mat Ryer <mat@stretchr.com>
2014-07-28 12:13:07 -0600

    added stop.Now example

--- stop/doc.go
+++ stop/doc.go
@@ -5,13 +5,20 @@
 // Stopper types when implementing the Stop method should use MakeChan
 // to create and return a stop channel, and pass stop.Done on the
 // channel once stopping has completed:
-//     func Stop() stop.Chan {
+//     func (t Type) Stop() stop.Chan {
 //       c := stop.MakeChan()
 //       go func(){
 //         // TODO: tear stuff down
 //         c <- stop.Done
 //       }()
 //       return c
+//     }
+//
+// If Stopper types do not need to do any background work, stop.Now() can
+// be returned, for example:
+//     func (t Type) Stop() stop.Chan {
+//       t.stopped = true
+//       return stop.Now()
 //     }
 //
 // Stopper types can be stopped in the following ways:
